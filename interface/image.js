5fce95dd87c392af47a2a202e2681219c71f8c61
wdrz <wtddrz@gmail.com>
2021-01-06 13:28:08 +0100

    Better visualization of image boxes

--- interface/image.js
+++ interface/image.js
@@ -1,8 +1,6 @@
-// VARIABLES
-const width = 500;
-// const imgSrc = 'https://c8.staticflickr.com/1/166/433557810_50fdb9ccc5_o.jpg';
-
 // DECLARATIONS
+
+const width = 500;
 
 const img = new Image();
 img.src = imgSrc;
@@ -17,21 +15,16 @@ const cellVal = (row, k) => row.cells[k].textContent * ratio;
 
 
 const drawRectangles = (ctx) => {
-  //iterate through rows
-  //rows would be accessed using the "row" variable assigned in the for loop
   for (let i = 1, row; row = table.rows[i]; i++) {
     ctx.beginPath(); 
     ctx.strokeStyle = randomColor();
-    console.log(ctx.strokeStyle);
     ctx.rect(cellVal(row, 1), cellVal(row, 2), cellVal(row, 3), cellVal(row, 4));
-    console.log(cellVal(row, 1), cellVal(row, 2), cellVal(row, 3), cellVal(row, 4));
+    ctx.fillText(row.cells[0].textContent, cellVal(row, 1), cellVal(row, 2)+10);
     ctx.stroke();
   }
 };
 
 // UPDATE
-
-
 
 const c = document.getElementById("canv1");
 const ctx = c.getContext("2d");
