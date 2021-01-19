1051588d856c62e9042b4113bd8cf9bf288e2718
wdrz <wtddrz@gmail.com>
2021-01-19 21:50:24 +0100

    "new Dataset" new mode: add all images

--- interface/actionAddDataset.php
+++ interface/actionAddDataset.php
@@ -12,6 +12,22 @@
     }
   }
 
+  function addAll($conn, $ds_id) {
+    $stmt = oci_parse($conn, "select image_id from Image");
+    own_execute($stmt, $conn);
+
+    while (($row = oci_fetch_array($stmt, OCI_BOTH))) {
+      $image_id = $row[0];
+
+      $stmt2 = oci_parse($conn, "insert into DatasetBelonging (image_id, dataset_id) values (:id1, :id2)");
+      oci_bind_by_name($stmt2, ':id1', $image_id, -1);
+      oci_bind_by_name($stmt2, ':id2', $ds_id, -1);
+      own_execute($stmt2, $conn);
+    }
+
+  }
+
+
   function run() {
     include("path.php");
 
@@ -49,85 +65,91 @@
 
     $ds_id = oci_fetch_array($stmt, OCI_BOTH)[0];
 
-    // add explicitly given ids to the dataset
-    for ($i = 1; !empty($_POST["IMGID${i}"]); $i++) {
-      $stmt = oci_parse($conn, "insert into DatasetBelonging (image_id, dataset_id) values (:id1, :id2)");
-      oci_bind_by_name($stmt, ':id1', $_POST["IMGID${i}"], -1);
-      oci_bind_by_name($stmt, ':id2', $ds_id , -1);
-      own_execute($stmt, $conn);
-    }
+
+    if ($_POST["RADIO"] === 'NOTALL') {
+
+      // add explicitly given ids to the dataset
+      for ($i = 1; !empty($_POST["IMGID${i}"]); $i++) {
+        $stmt = oci_parse($conn, "insert into DatasetBelonging (image_id, dataset_id) values (:id1, :id2)");
+        oci_bind_by_name($stmt, ':id1', $_POST["IMGID${i}"], -1);
+        oci_bind_by_name($stmt, ':id2', $ds_id , -1);
+        own_execute($stmt, $conn);
+      }
 
 
-    // iterate over privided names of labels
-    for ($i = 1; !empty($_POST["LABEL${i}"]) && !empty($_POST["COUNT${i}"]); $i++) {
-      // find id of given label
-      $stmt = oci_parse($conn, "select label_id from label where name = :label");
-      oci_bind_by_name($stmt, ':label', $_POST["LABEL${i}"], -1);
-      own_execute($stmt, $conn);
-      $label_id = oci_fetch_array($stmt, OCI_BOTH)[0];
+      // iterate over privided names of labels
+      for ($i = 1; !empty($_POST["LABEL${i}"]) && !empty($_POST["COUNT${i}"]); $i++) {
+        // find id of given label
+        $stmt = oci_parse($conn, "select label_id from label where name = :label");
+        oci_bind_by_name($stmt, ':label', $_POST["LABEL${i}"], -1);
+        own_execute($stmt, $conn);
+        $label_id = oci_fetch_array($stmt, OCI_BOTH)[0];
 
-      // select images that contain boxes classified this way
-      $stmt = oci_parse($conn, "
-        select image_id, count(area_id)
-        from (ImageArea natural join Classification)
-        where label_id = :lid
-        group by image_id
-      ");
-      oci_bind_by_name($stmt, ':lid', $label_id, -1);
-      own_execute($stmt, $conn);
+        // select images that contain boxes classified this way
+        $stmt = oci_parse($conn, "
+          select image_id, count(area_id)
+          from (ImageArea natural join Classification)
+          where label_id = :lid
+          group by image_id
+        ");
+        oci_bind_by_name($stmt, ':lid', $label_id, -1);
+        own_execute($stmt, $conn);
 
-      $num_added = 0;
-      while (($row = oci_fetch_array($stmt, OCI_BOTH))) {
-        if ($num_added >= $_POST["COUNT${i}"]) {
-          break;
+        $num_added = 0;
+        while (($row = oci_fetch_array($stmt, OCI_BOTH))) {
+          if ($num_added >= $_POST["COUNT${i}"]) {
+            break;
+          }
+          $image_id = $row[0];
+          $count = $row[1];
+
+          $stmt2 = oci_parse($conn, "insert into DatasetBelonging (image_id, dataset_id) values (:id1, :id2)");
+          oci_bind_by_name($stmt2, ':id1', $image_id, -1);
+          oci_bind_by_name($stmt2, ':id2', $ds_id , -1);
+          own_execute($stmt2, $conn);
+
+          $num_added = $count + $num_added;
+
         }
-        $image_id = $row[0];
-        $count = $row[1];
-
-        $stmt2 = oci_parse($conn, "insert into DatasetBelonging (image_id, dataset_id) values (:id1, :id2)");
-        oci_bind_by_name($stmt2, ':id1', $image_id, -1);
-        oci_bind_by_name($stmt2, ':id2', $ds_id , -1);
-        own_execute($stmt2, $conn);
-
-        $num_added = $count + $num_added;
 
       }
 
-    }
+      // copy images from other datasets
+      for ($i = 1; !empty($_POST["DS${i}"]); $i++) {
+        // find id of given label
+        $stmt = oci_parse($conn, "select image_id from DatasetBelonging where dataset_id = :did");
+        oci_bind_by_name($stmt, ':did', $_POST["DS${i}"], -1);
+        own_execute($stmt, $conn);
 
-    // copy images from other datasets
-    for ($i = 1; !empty($_POST["DS${i}"]); $i++) {
-      // find id of given label
-      $stmt = oci_parse($conn, "select image_id from DatasetBelonging where dataset_id = :did");
-      oci_bind_by_name($stmt, ':did', $_POST["DS${i}"], -1);
-      own_execute($stmt, $conn);
+        while (($row = oci_fetch_array($stmt, OCI_BOTH))) {
+          $image_id = $row[0];
 
-      while (($row = oci_fetch_array($stmt, OCI_BOTH))) {
-        $image_id = $row[0];
-
-        $stmt2 = oci_parse($conn, "insert into DatasetBelonging (image_id, dataset_id) values (:id1, :id2)");
-        oci_bind_by_name($stmt2, ':id1', $image_id, -1);
-        oci_bind_by_name($stmt2, ':id2', $ds_id , -1);
-        own_execute($stmt2, $conn);
+          $stmt2 = oci_parse($conn, "insert into DatasetBelonging (image_id, dataset_id) values (:id1, :id2)");
+          oci_bind_by_name($stmt2, ':id1', $image_id, -1);
+          oci_bind_by_name($stmt2, ':id2', $ds_id , -1);
+          own_execute($stmt2, $conn);
+        }
       }
-    }
 
-    // delete duplicates in this dataset
-    $stmt = oci_parse($conn, "
-      delete DatasetBelonging
-      where (
-        rowid not in (
-          select min(rowid)
-          from   DatasetBelonging
-          group  by image_id, dataset_id
-        ) and (
-          dataset_id = :did
+      // delete duplicates in this dataset
+      $stmt = oci_parse($conn, "
+        delete DatasetBelonging
+        where (
+          rowid not in (
+            select min(rowid)
+            from   DatasetBelonging
+            group  by image_id, dataset_id
+          ) and (
+            dataset_id = :did
+          )
         )
-      )
-    ");
-    oci_bind_by_name($stmt, ':did', $ds_id , -1);
-    own_execute($stmt, $conn);
+      ");
+      oci_bind_by_name($stmt, ':did', $ds_id , -1);
+      own_execute($stmt, $conn);
+    } else if ($_POST["RADIO"] ===  'ALL') {
+      addAll($conn, $ds_id);
 
+    }
     oci_commit($conn);
 
     header("Location: ".$path);
